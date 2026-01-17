6d03fd6e3876184011443cf54c1d57f35d10676b
vedant-rmgd <vedant.ramgade@gmail.com>
2026-01-17 23:54:17 +0530

    create a resume preview page to display shared resume and create loader component

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyResumeData } from "../assets/assets";
+import Loader from "../components/Loader.jsx";
+import ResumePreview from "../components/ResumePreview.jsx";
+import { ArrowLeftIcon } from "lucide-react";
 
 function Preview() {
-  return (
-    <div>Preview</div>
-  )
+    const { resumeId } = useParams();
+    const [isloading, setIsLoading] = useState(true);
+    const [resumeData, setResumeData] = useState(null);
+
+    const loadResume = async () => {
+        setResumeData(
+            dummyResumeData.find((resume) => resume._id === resumeId || null)
+        );
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        loadResume();
+    }, []);
+
+    return resumeData ? (
+        <div className="bg-slate-100">
+            <div className="max-w-3xl mx-auto py-10">
+                <ResumePreview
+                    data={resumeData}
+                    template={resumeData.template}
+                    accentColor={resumeData.accent_color}
+                    classes="py-4 bg-white"
+                />
+            </div>
+        </div>
+    ) : (
+        <div>
+            {isloading ? (
+                <Loader />
+            ) : (
+                <div className="flex flex-col items-center justify-center h-screen">
+                    <p className="text-center text-5xl text-slate-400 font-medium">
+                        Resume not found!
+                    </p>
+                    <a
+                        href="/"
+                        className="mt-6 bg-green-500 hover:bg-green-600 text-white rounded-full px-6 h-9 m-1 ring-offset-1 ring-1 ring-green-400 flex items-center transition-colors"
+                    >
+                        <ArrowLeftIcon className="m-2 size-4" />
+                        go to home page
+                    </a>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default Preview
+export default Preview;
