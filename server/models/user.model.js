9447ea2ad8657bc97aac05eb5229130ef9ecda1d
vedant-rmgd <vedant.ramgade@gmail.com>
2026-01-24 22:38:05 +0530

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
