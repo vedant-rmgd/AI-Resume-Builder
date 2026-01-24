57d184d2a8a5ef590e4dfc2a043cba0eedc7b945
vedant-rmgd <vedant.ramgade@gmail.com>
2026-01-24 22:26:38 +0530

    database connected successfully

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,25 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        
+        mongoose.connection.on("connected", () => {
+            console.log("Database connected successfully 🥳🥳🥳");
+        });
+
+        let mongodbURI = process.env.MONGODB_URI;
+        const projectName = "resume-builder";
+
+        if (!mongodbURI) {
+            throw new Error("MONGODB_URI environmental variable not set");
+        }
+
+        if (mongodbURI.endsWith("/")) {
+            mongodbURI = mongodbURI.slice(0, -1);
+        }
+
+        await mongoose.connect(`${mongodbURI}/${projectName}`);
     } catch (error) {
-        
+        console.error("Error connecting to mongoDB : ", error);
     }
-}
+};
+
+export default connectDB
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//database connection
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
