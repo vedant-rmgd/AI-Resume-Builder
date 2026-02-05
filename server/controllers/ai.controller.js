718ac510dce340c352a2e635706fbf8c3a10b1d0
vedant-rmgd <vedant.ramgade@gmail.com>
2026-02-05 14:03:45 +0530

    create enhance job description controller

--- server/controllers/ai.controller.js
+++ server/controllers/ai.controller.js
@@ -41,3 +41,43 @@ export const enhanceProfessionalSummery = async (req, res) => {
         throw new apiError(400, error?.message);
     }
 };
+
+export const enhanceJobDescription = async (req, res) => {
+    try {
+        const { userContent } = req.body;
+
+        if (!userContent) {
+            throw new apiError(400, "Missing required field");
+        }
+
+        const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are an expert in resume writing. Your task is to enhance the job description of a resume. The job description should be 1-2 sentences also highlighting key responsibilities and achivements. Use action verb and quantifiable resuit where possible. Make it ATS-friendly. And only return text no options or anything else.",
+                },
+                {
+                    role: "user",
+                    content: userContent,
+                },
+            ],
+        });
+
+        const enhancedContent = response.choices[0].message.content;
+        console.log("Response from AI : ", enhancedContent);
+
+        return res
+            .status(200)
+            .json(
+                new apiResponse(
+                    200,
+                    enhancedContent,
+                    "Responce from AI is successfully generated!",
+                ),
+            );
+    } catch (error) {
+        throw new apiError(400, error?.message);
+    }
+};
