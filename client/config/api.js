5c5394d0b379d273b18d143a6df2b407f8275b89
vedant-rmgd <vedant.ramgade@gmail.com>
2026-03-02 19:17:17 +0530

    made production ready

--- client/config/api.js
+++ client/config/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.DEV
+        ? "http://localhost:3000"
+        : "/",
 });
 
 export default api;
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import userRouter from "./routes/user.routes.js";
 import resumeRouter from "./routes/resume.routes.js";
 import aiRouter from "./routes/ai.routes.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,10 +16,6 @@ await connectDB();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Server is live....!!!");
-});
-
 //ROUTES->
 app.use("/api/users", userRouter);
 app.use("/api/resumes", resumeRouter);
@@ -26,4 +23,24 @@ app.use("/api/ai", aiRouter);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
+});
+
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../client/dist/index.html"))
+  );
+}
+
+// Optional root test route
+app.get("/", (req, res) => {
+  res.send("Server is live....!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
